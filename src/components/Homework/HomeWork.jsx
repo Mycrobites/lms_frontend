8c9f12d419e12cc8e6941c61a39aa35a937ab5cc
Naga Harshita Marupaka <harshita@Nagas-MacBook-Pro.local>
2021-03-14 00:09:45 +0530

    Added homework

--- src/components/Homework/HomeWork.jsx
+++ src/components/Homework/HomeWork.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HomeWork.css'
+import { PieChart, Pie,  Cell } from 'recharts';
+
+const Subject = (props) =>{
+    const data=
+        [
+            {title: props.name, value: props.percent, color:'#E38627'},
+            {title: '', value: 100-props.percent, color:'#ffffff'}
+        ];
+    
+    return(
+        <div className="Subject">
+            <div> 
+                <PieChart width={100} height={100}>
+                    <text
+                        x={50}
+                        y={50}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                    >
+                        {props.percent}
+                    </text>
+                    <Pie
+                        data={data}
+                        dataKey="value"
+                        innerRadius="80%"
+                        outerRadius="100%"
+                        fill="#CCC"
+                        startAngle={90}
+                        endAngle={-270}
+                        paddingAngle={0}
+                        cornerRadius={5}
+                    >
+                        <Cell
+                        key="test"
+                        
+                        fill="#82ca9d"
+                        />
+                    </Pie>
+                    </PieChart>
+                </div>
+            <div className="timing1">
+                <div style={{paddingTop: "15px"}}><h2>{props.name}</h2><br/>
+                    Sep 7, 2021
+                </div>
+                   
+            
+        </div>
+        </div>
+    )
+}
 
 const HomeWork = () => {
+    const [subjects] = useState([{id: 1, name: "Biology", percent:70}, {id: 2, name: "Physics", percent:80}, {id: 3,name: "Maths", percent: 90}])
     return (
-        <div>
-            
+        <div className="box1">
+            <h1>Homework Progress</h1>
+            <div className="subject">
+                {subjects.map((subject) => (
+                <Subject key={subject.id} {...subject} />
+                ))}
+            </div>
         </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Courses from '../../components/Courses/Courses'
 import LeaderBoard from '../../components/LeaderBoard/LeaderBoard'
 import UpcomingLessons from '../../components/UpcomingLessons/UpcomingLessons'
 import Task from '../../components/Task/Task'
+import Homework from '../../components/Homework/HomeWork'
 import './Home.css'
 
 const Home = () => {
@@ -14,6 +15,7 @@ const Home = () => {
             <Courses/>
             <CalendarComponent />
             <Task/>
+            <Homework/>
         </div>
     )
 }

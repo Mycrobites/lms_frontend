ee6a027d59c06d21e50b11d378848fee260063fb
Vedant Singh <67269716+imvsr-69@users.noreply.github.com>
2021-05-10 19:52:03 +0530

    few changes

--- src/App.js
+++ src/App.js
@@ -1,66 +1,66 @@
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import { useContext } from "react";
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+} from 'react-router-dom';
+import { useContext } from 'react';
 // import Home from "./pages/Home/Home";
-import DashBoard from "./pages/Dashboard/DashBoard";
-import Enrollment from "./pages/Enrollments/Enrollment";
-import Achievement from "./pages/Achievements/Achievement";
-import Forum from "./pages/Forum/Forum";
-import ErrorPage from "./pages/ErrorPage/ErrorPage";
-import NavBar from "./components/NavBar/NavBar";
-import ProfilePage from "./pages/Profile/ProfilePage";
-import Login from "./pages/LoginPage/Login";
-import CoursePage from "./pages/CoursePage/CoursePage";
-import UserContext from "./context/authContext";
-import Assignment from "./pages/AssignmentPage/Assignment";
-import QuizPage from "./pages/QuizPage/QuizPage2";
+import DashBoard from './pages/Dashboard/DashBoard';
+import Enrollment from './pages/Enrollments/Enrollment';
+import Achievement from './pages/Achievements/Achievement';
+import Forum from './pages/Forum/Forum';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import NavBar from './components/NavBar/NavBar';
+import ProfilePage from './pages/Profile/ProfilePage';
+import Login from './pages/LoginPage/Login';
+import CoursePage from './pages/CoursePage/CoursePage';
+import UserContext from './context/authContext';
+import Assignment from './pages/AssignmentPage/Assignment';
+import QuizPage from './pages/QuizPage/QuizPage2';
 
 const App = () => {
-  const { userDetails } = useContext(UserContext);
+	const { userDetails } = useContext(UserContext);
 
-  return (
-    <Router>
-      <div className="App">
-        {userDetails && <NavBar />}
-        <Switch>
-          <Route exact path="/">
-            {!userDetails ? <Login /> : <Redirect to="/dashboard" />}
-          </Route>
-          <Route exact path="/dashboard">
-            {userDetails ? <DashBoard /> : <Redirect to="/" />}
-          </Route>
-          <Route exact path="/enrollment">
-            {userDetails ? <Enrollment /> : <Redirect to="/" />}
-          </Route>
-          <Route exact path="/achievement">
-            <Achievement />
-          </Route>
-          <Route exact path="/forum">
-            {userDetails ? <Forum /> : <Redirect to="/" />}
-          </Route>
-          <Route exact path="/profile">
-            {userDetails ? <ProfilePage /> : <Redirect to="/" />}
-          </Route>
-          <Route exact path="/course/:id">
-            {userDetails ? <CoursePage /> : <Redirect to="/" />}
-          </Route>
-          <Route exact path="/quiz/:id">
-            {userDetails ? <QuizPage /> : <Redirect to="/" />}
-          </Route>
-          <Route exact path="/assignment/:id">
-            {userDetails ? <Assignment /> : <Redirect to="/" />}
-          </Route>
-          <Route path="*">
-            <ErrorPage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				{userDetails && <NavBar />}
+				<Switch>
+					<Route exact path="/">
+						{!userDetails ? <Login /> : <Redirect to="/dashboard" />}
+					</Route>
+					<Route exact path="/dashboard">
+						{userDetails ? <DashBoard /> : <Redirect to="/" />}
+					</Route>
+					<Route exact path="/enrollment">
+						{userDetails ? <Enrollment /> : <Redirect to="/" />}
+					</Route>
+					<Route exact path="/achievement">
+						{userDetails ? <Achievement /> : <Redirect to="/" />}
+					</Route>
+					<Route exact path="/forum">
+						{userDetails ? <Forum /> : <Redirect to="/" />}
+					</Route>
+					<Route exact path="/profile">
+						{userDetails ? <ProfilePage /> : <Redirect to="/" />}
+					</Route>
+					<Route exact path="/course/:id">
+						{userDetails ? <CoursePage /> : <Redirect to="/" />}
+					</Route>
+					<Route exact path="/quiz/:id">
+						{userDetails ? <QuizPage /> : <Redirect to="/" />}
+					</Route>
+					<Route exact path="/assignment/:id">
+						{userDetails ? <Assignment /> : <Redirect to="/" />}
+					</Route>
+					<Route path="*">
+						<ErrorPage />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,103 +1,103 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react';
 
 const UserContext = createContext();
 
 const getUserDetails = () => {
-  const user = localStorage.getItem("user-details");
-  if (user) {
-    return JSON.parse(user);
-  } else {
-    return null;
-  }
+	const user = localStorage.getItem('user-details');
+	if (user) {
+		return JSON.parse(user);
+	} else {
+		return null;
+	}
 };
 
 const getUserQuiz = () => {
-  const quiz = sessionStorage.getItem("user-current-quiz");
-  if (quiz) {
-    return JSON.parse(quiz);
-  } else {
-    return null;
-  }
+	const quiz = sessionStorage.getItem('user-current-quiz');
+	if (quiz) {
+		return JSON.parse(quiz);
+	} else {
+		return null;
+	}
 };
 
 const checkIsTestSubmitted = () => {
-  const quiz = sessionStorage.getItem("test-submitted");
-  if (quiz) {
-    return JSON.parse(quiz);
-  } else {
-    return null;
-  }
+	const quiz = sessionStorage.getItem('test-submitted');
+	if (quiz) {
+		return JSON.parse(quiz);
+	} else {
+		return null;
+	}
 };
 
 const getUserProfilePic = () => {
-  const pic = localStorage.getItem("user-profile-pic");
-  if (pic) {
-    return JSON.parse(pic);
-  } else {
-    return null;
-  }
+	const pic = localStorage.getItem('user-profile-pic');
+	if (pic) {
+		return JSON.parse(pic);
+	} else {
+		return null;
+	}
 };
 
 export const UserContextProvider = (props) => {
-  const [userDetails, setUserDetails] = useState(getUserDetails);
-  const [userCurrentQuiz, setUserCurrentQuiz] = useState(getUserQuiz);
-  const [isTestSubmitted, setIsTestSubmitted] = useState(checkIsTestSubmitted);
-  const [userProfilePic, setUserProfilePic] = useState(getUserProfilePic);
+	const [userDetails, setUserDetails] = useState(getUserDetails);
+	const [userCurrentQuiz, setUserCurrentQuiz] = useState(getUserQuiz);
+	const [isTestSubmitted, setIsTestSubmitted] = useState(checkIsTestSubmitted);
+	const [userProfilePic, setUserProfilePic] = useState(getUserProfilePic);
 
-  const updateUser = (data) => {
-    setUserDetails(data);
-    localStorage.setItem("user-details", JSON.stringify(data));
-  };
+	const updateUser = (data) => {
+		setUserDetails(data);
+		localStorage.setItem('user-details', JSON.stringify(data));
+	};
 
-  const removeUser = () => {
-    setUserDetails(null);
-    sessionStorage.clear();
-    localStorage.clear();
-  };
+	const removeUser = () => {
+		setUserDetails(null);
+		sessionStorage.clear();
+		localStorage.clear();
+	};
 
-  const updateUserProfilePic = (pic) => {
-    setUserProfilePic(pic);
-    localStorage.setItem("user-profile-pic", JSON.stringify({ profile: pic }));
-  };
+	const updateUserProfilePic = (pic) => {
+		setUserProfilePic({ profile: pic });
+		localStorage.setItem('user-profile-pic', JSON.stringify({ profile: pic }));
+	};
 
-  const addQuiz = (id, duration, test_time) => {
-    setUserCurrentQuiz({ id, duration, test_time });
-  };
+	const addQuiz = (id, duration, test_time) => {
+		setUserCurrentQuiz({ id, duration, test_time });
+	};
 
-  const timeUpdate = () => {
-    setUserCurrentQuiz({
-      ...userCurrentQuiz,
-      test_time: userCurrentQuiz?.test_time - 1000,
-    });
-    sessionStorage.setItem(
-      "user-current-quiz",
-      JSON.stringify(userCurrentQuiz)
-    );
-  };
+	const timeUpdate = () => {
+		setUserCurrentQuiz({
+			...userCurrentQuiz,
+			test_time: userCurrentQuiz?.test_time - 1000,
+		});
+		sessionStorage.setItem(
+			'user-current-quiz',
+			JSON.stringify(userCurrentQuiz),
+		);
+	};
 
-  const submitTest = () => {
-    setIsTestSubmitted(true);
-    sessionStorage.setItem("test-submitted", true);
-  };
+	const submitTest = () => {
+		setIsTestSubmitted(true);
+		sessionStorage.setItem('test-submitted', true);
+	};
 
-  return (
-    <UserContext.Provider
-      value={{
-        userDetails,
-        updateUser,
-        removeUser,
-        addQuiz,
-        userCurrentQuiz,
-        timeUpdate,
-        submitTest,
-        isTestSubmitted,
-        userProfilePic,
-        updateUserProfilePic,
-      }}
-    >
-      {props.children}
-    </UserContext.Provider>
-  );
+	return (
+		<UserContext.Provider
+			value={{
+				userDetails,
+				updateUser,
+				removeUser,
+				addQuiz,
+				userCurrentQuiz,
+				timeUpdate,
+				submitTest,
+				isTestSubmitted,
+				userProfilePic,
+				updateUserProfilePic,
+			}}
+		>
+			{props.children}
+		</UserContext.Provider>
+	);
 };
 
 export default UserContext;
